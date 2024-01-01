9572e49db6ab0061f79cbff88d0acb365bc37982
dynamiteOpanty <103298123+dynamiteOpanty@users.noreply.github.com>
2024-01-01 10:01:50 +0900

    moduleを使用するように

--- automatas/automata3.py
+++ automatas/automata3.py
@@ -1,25 +1,24 @@
+from backyard.module import ReadText, WriteTextLines, ReadCSV, WriteCSV, ReadJSON
 from pathlib import Path
 
 # VOの各フォルダの、sub_enの順番をソートするpythonプログラムです
 # ソートはsub_en内の各接頭辞単位で行われます
 # 使用しません　depricated　個人的な記録として残します
 
-dir = "./automata/"
-excludesname = "excludes"
-noSubname = "no_JP_sub_list"
-excludespath = f"{dir}/{excludesname}.txt"
-noSubList = f"{dir}/{noSubname}.txt"
-outputFileName = "sub_en_sorted.csv"
+config = ReadJSON("./automatas/backyard/config.json")
+EXCLUDES_PATH = f"./automatas/backyard/{config['excludeFileName']}.txt"
+NOSUB_LIST = f"./automatas/backyard/{config['noSubList']}.txt"
+OUTPUTFILE_NAME = "sub_en_sorted.csv"
 
 def main():
     VO = Path("./VO")
     try:
-        excludes = ReadText(excludespath)
+        excludes = ReadText(EXCLUDES_PATH)
     except:
-        WriteTextLines(excludespath)
+        WriteTextLines(EXCLUDES_PATH)
         excludes = ""
     try:
-        noSub = ReadText(noSubList)
+        noSub = ReadText(NOSUB_LIST)
     except:
         print("noSub list wasn't found")
         exit(-1)
@@ -57,7 +56,7 @@ def main():
                 group3 = [record[0] for record in sort2D(group2)]
                 result += group3
             result.insert(0, ["Key", "Dialogue", "Context"])
-            WriteCSV(f"{folder}/{outputFileName}", result)
+            WriteCSV(f"{folder}/{OUTPUTFILE_NAME}", result)
     print("done")
 
 def sort2D(group: list):
@@ -72,27 +71,6 @@ def ReadText(path: str):
         for line in file:
             result.append(line.replace("\n", ""))
         return result
-
-def WriteTextLines(path: str, text=""):
-    with open(file=path, mode="w", encoding="utf-8") as file:
-        for line in text:
-            file.write(f"{line}\n")
-
-def ReadCSV(path: str):
-    from csv import reader
-    result = []
-    with open(path, "r", encoding="UTF-8") as file:
-        row = reader(file)
-        for cells in row:
-            result.append(cells)
-        return result
-
-
-def WriteCSV(path: str, list2D: list[list[str]]):
-    from csv import writer
-    with open(path, "w", encoding="UTF-8") as file:
-        writer2 = writer(file, delimiter=",")
-        writer2.writerows(list2D)
 
 if __name__ == "__main__":
     # main()
