5d4e83e0dc64b5b8e01fad119034db0a7a532aa3
dynamiteOpanty <103298123+dynamiteOpanty@users.noreply.github.com>
2024-09-14 19:20:58 +0900

    automata6

---
+++ automatas/automata6.py
@@ -0,0 +1,64 @@
+from backyard.module import ReadJSON, ReadCSV, ReadText, WriteTextLines
+from pathlib import Path
+
+config = ReadJSON("./automatas/backyard/config.json")
+EXCLUDES_PATH = f"./automatas/backyard/{config['excludeFileName']}.txt"
+NOSUB_LIST = f"./automatas/backyard/{config['noSubList']}.txt"
+
+
+def main():
+    VO = Path("./VO")
+    try:
+        excludes = ReadText(EXCLUDES_PATH)
+    except:
+        WriteTextLines(EXCLUDES_PATH)
+        excludes = ""
+    try:
+        noSub = ReadText(NOSUB_LIST)
+    except:
+        print("noSub list wasn't found")
+        exit(-1)
+    folders = [folder for folder in VO.iterdir() if folder.is_dir()]
+    for folder in folders:
+        if folder.name in excludes or folder.name in noSub:
+            continue
+        for file in folder.iterdir():
+            if file.name == "sub_jp.csv":
+                subJpPath = file
+            elif file.name == "sub_en.csv":
+                subEnPath = file
+            continue
+        try:
+            type(subJpPath)
+            type(subEnPath)
+        except:
+            print("error. either sub_jp or sub_en wasn't found.")
+            exit(-1)
+        subEn = ReadCSV(subEnPath.absolute())
+        subJp = ReadCSV(subJpPath.absolute())
+        if not len(subEn) == len(subJp):
+            print(f"\033[31mwrong range at {folder.name}!\033[0m")
+            print(f"├─ JP len {len(subJp)}")
+            print(f"└─ JP len {len(subEn)}")
+        discrepancy = []
+        valid = True
+        for lineEn in subEn:
+            valid = False
+            for lineJp in subJp:
+                if lineEn[0] == lineJp[0]:
+                    valid = True
+            if valid:
+                continue
+            discrepancy.append(lineEn[0])
+        if 0 < len(discrepancy):
+            print(f"\033[33mdiscrepancy at {folder.name}!\033[0m")
+            for line in discrepancy:
+                if line == discrepancy[-1]:
+                    prefix = "└─"
+                else:
+                    prefix = "├─"
+                print(f"{prefix} {line}")
+            # print(f"└─ {len(discrepancy)}")
+
+if __name__ == "__main__":
+    main()
